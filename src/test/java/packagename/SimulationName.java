4538c58fde9228c596ce70c8e4fa5e718abbf3c5
katwaGatling <katwa@gatling.io>
2024-03-25 14:37:28 +0100

    Retrieve AWS Secret Manager

--- src/test/java/packagename/SimulationName.java
+++ src/test/java/packagename/SimulationName.java
@@ -5,10 +5,19 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 public class SimulationName extends Simulation {
 
   public static final int vu = Integer.getInteger("vu", 1);
+
+  public static final ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
 
   HttpProtocolBuilder httpProtocol =
       http.baseUrl("https://computer-database.gatling.io")
@@ -18,9 +27,57 @@ public class SimulationName extends Simulation {
           .userAgentHeader(
               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0");
 
-  ScenarioBuilder users = scenario("Scenario 1").exec(http("Home").get("/computers"));
+  ScenarioBuilder readSecret =
+      scenario("Read Secret")
+          .exec(
+              session -> {
+                String secretName = "my-secret-name";
+                Region region = Region.of("eu-west-3");
+
+                AwsBasicCredentials awsCredentials =
+                    AwsBasicCredentials.create(
+                        System.getenv("AWS_ACCESS_KEY_ID"), System.getenv("AWS_SECRET_ACCESS_KEY"));
+
+                SecretsManagerClient client =
+                    SecretsManagerClient.builder()
+                        .region(region)
+                        .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                        .build();
+
+                GetSecretValueRequest getSecretValueRequest =
+                    GetSecretValueRequest.builder().secretId(secretName).build();
+                GetSecretValueResponse getSecretValueResponse;
+
+                try {
+                  getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+                } catch (Exception e) {
+                  throw new RuntimeException(e);
+                }
+
+                String secret = getSecretValueResponse.secretString();
+                queue.offer(secret);
+                return session;
+              });
+
+  ScenarioBuilder users =
+      scenario("Scenario 1")
+          .exec(
+              exec(
+                  session -> {
+                    String secretString = queue.poll();
+
+                    if (secretString != null) {
+                      System.out.println("Retrieved secret string: " + secretString);
+                    } else {
+                      System.out.println("No secret retrieved");
+                      exitHere();
+                    }
+                    return session;
+                  }),
+              http("Home").get("/computers"));
 
   {
-    setUp(users.injectOpen(atOnceUsers(vu))).protocols(httpProtocol);
+    setUp(readSecret.injectOpen(atOnceUsers(1)).andThen(users.injectOpen(atOnceUsers(vu))))
+        .protocols(httpProtocol);
   }
 }
